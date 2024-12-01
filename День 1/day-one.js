fbcf7d9a12dd4f81be02d5ad0e0b04ad3c826742
Korzher <tchuprinin.constantin@yandex.ru>
2024-12-01 17:40:28 +0300

    first day both puzzles

--- День 1/day-one.js
+++ День 1/day-one.js
@@ -31,5 +31,25 @@ diffMap = diff.map((el) => {
 });
 
 let value = diffMap.reduce((sum, el) => sum + el, 0);
+console.log(`Первое число: ${value}`);
 
-console.log(value);
+let count = 0;
+let similarity = [];
+for (i = 0; i <= firstSorted.length - 1; i++) {
+  for (j = 0; j <= secondSorted.length - 1; j++) {
+    if (firstSorted[i] === secondSorted[j]) {
+      count++;
+    } else if (firstSorted[i] > secondSorted[j]) {
+      continue;
+    }
+  }
+  if (count === 0) {
+    continue;
+  }
+  similarity.push(firstSorted[i] * count);
+  count = 0;
+}
+
+let result = similarity.reduce((sum, el) => sum + el, 0);
+
+console.log(`Второе число: ${result}`);
